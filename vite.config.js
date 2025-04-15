89a5d89a44acb16de1326257fa4173fe41cdceb8
George <gosha3484@gmail.com>
2025-04-15 13:18:43 +0300

    added custom-hooks

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/~georgiia/WSK/hooks/',
+  base: '/~georgiia/WSK/custom-hooks/',
 });
